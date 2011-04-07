2c098ea7bfe8bba35d02f65960ff179209117260
Sebastian Marr <sbstnmrr@googlemail.com>
2011-04-07 22:06:19 +0200

    using system's webster dictionary for testing purposes

--- tc_burstsort.rb
+++ tc_burstsort.rb
@@ -5,18 +5,18 @@ class BurstsortTest < Test::Unit::TestCase
   
   def setup
     # collect an array of words from dictionary files
-    @dict = (File.new("dictwords").readlines.inject([]) { |test, line| test << line.split; }).shuffle.flatten
     @hamlet = (File.new("hamletwords").readlines.inject([]) { |test, line| test << line.split; }).shuffle.flatten
+    @web2 = (File.new("/usr/share/dict/words").readlines.inject([]) { |test, line| test << line.split; }).shuffle.flatten
     #extract all uniqe characters from input words and create a sorted alphabet of them
-    @hamletalphabet = (@hamlet.join.chars.to_a.uniq << "").sort
-    @dictalphabet = (@dict.join.chars.to_a.uniq << "").sort
-  end
-  
-  def test_sort_dict
-    assert_equal(@dict.sort, burstsort(@dict, @dictalphabet, 1000))
+    @hamlet_alphabet = (@hamlet.join.chars.to_a.uniq << "").sort
+    @web2_alphabet = (@web2.join.chars.to_a.uniq << "").sort
   end
   
   def test_sort_hamlet
-    assert_equal(@hamlet.sort, burstsort(@hamlet, @hamletalphabet, 1000))
+    assert_equal(@hamlet.sort, burstsort(@hamlet, @hamlet_alphabet, 1000))
+  end
+  
+  def test_sort_web2
+    assert_equal(@web2.sort, burstsort(@web2, @web2_alphabet, 8192))
   end
 end
