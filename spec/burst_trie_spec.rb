981f11e5c265cd5732048be64d3dc4814f248bcb
Sebastian Marr <sbstnmrr@googlemail.com>
2012-01-30 13:54:11 +0100

    convert trie tests to rspec

--- spec/burst_trie_spec.rb
+++ spec/burst_trie_spec.rb
@@ -3,7 +3,26 @@ require "burstsort"
 describe BurstSort do
   
   describe BurstSort::BurstTrie do
-    it "should burst on the given limit"
-    it "should reject too many duplicates for the given limit"
+    
+    before(:all) do
+      # collect an array of words from dictionary files
+      @hamlet = (File.new("spec/data/hamletwords").readlines.inject([]) { |test, line| test << line.split; }).shuffle.flatten
+      # extract all uniqe characters from input words and create a sorted alphabet of them
+      @hamletalphabet = (@hamlet.join.chars.to_a.uniq << "").sort
+    end
+    
+    it "should burst on the given limit" do
+      t = BurstSort::BurstTrie.new(@hamletalphabet, 1000)
+      @hamlet.each { |w| t.insert(w) }
+      t.containers.each do |c|
+        c.length.should be <= 1000
+      end
+    end
+    
+    it "should reject too many duplicates for the given limit" do
+      t = BurstSort::BurstTrie.new(@hamletalphabet, 1)
+      lambda { 2.times {t.insert "foo" } }.should raise_exception(ArgumentError)
+    end
+    
   end
 end
