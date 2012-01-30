ded673fcd75f1eba3a373acf507f9984e41a78ee
Sebastian Marr <sbstnmrr@googlemail.com>
2012-01-30 13:54:11 +0100

    convert sort tests to rspec

--- spec/burstsort_spec.rb
+++ spec/burstsort_spec.rb
@@ -2,8 +2,21 @@ require "burstsort"
 
 describe "algorithm" do
   
-  it "should sort the strings of hamlet"
+  before(:all) do
+    # collect an array of words from dictionary files
+    @hamlet = (File.new("spec/data/hamletwords").readlines.inject([]) { |test, line| test << line.split; }).shuffle.flatten
+    @words = (File.new("/usr/share/dict/words").readlines.inject([]) { |test, line| test << line.split; }).shuffle.flatten
+    #extract all uniqe characters from input words and create a sorted alphabet of them
+    @hamlet_alphabet = (@hamlet.join.chars.to_a.uniq << "").sort
+    @words_alphabet = (@words.join.chars.to_a.uniq << "").sort
+  end
   
-  it "should sort the unix dictionary file"
+  it "should sort the strings of hamlet" do
+    BurstSort::run(@hamlet, @hamlet_alphabet, 1000).should == @hamlet.sort
+  end
+  
+  it "should sort the unix dictionary file" do
+    BurstSort::run(@words, @words_alphabet, 1000).should == @words.sort
+  end
   
 end
